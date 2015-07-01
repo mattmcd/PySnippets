15233bc9735762a13e4057bb7b05555f3f83ad0b
mattmcd <matt@matt-mcdonnell.com>
2015-07-01 09:58:22 +0100

    A few more variations trying different techniques

--- Sieve/sieve.py
+++ Sieve/sieve.py
@@ -1,4 +1,8 @@
+from itertools import ifilter
+import numpy as np
+
 def sieve(n):
+    '''Recursive Sieve of Eratosthenes'''
     acc = []
     def aux(numList):
         try:
@@ -11,6 +15,7 @@ def sieve(n):
     return acc
 
 def sieve2(n):
+    '''Iterative Sieve of Eratosthenes'''
     acc = []
     numList = xrange(2,n)
     while numList:
@@ -19,6 +24,32 @@ def sieve2(n):
         numList = filter(lambda x: x % k, numList)
     return acc
 
+def sieve3(n):
+    '''Iterative Sieve of Eratosthenes using iterator'''
+    acc = [] 
+    numList = iter(xrange(2,n))
+    try:
+        while True:
+            k = numList.next()
+            acc.append(k)
+            numList = ifilter(lambda x: # all(x % np.array(acc))
+                reduce(lambda acc,a: acc and (x % a), acc, True)
+                , numList)
+    except StopIteration:
+        pass
+    return acc
+
+def sieve4(n):
+    '''Sieve of Eratosthenes in memory'''
+    acc = []
+    sieve_array = np.arange(2,n)
+    next_index = 0
+    for (i,k) in enumerate(sieve_array):
+        if k != 0:
+          sieve_array[sieve_array % k == 0] = 0
+          sieve_array[i] = k # Store the prime back into the array 
+    return sieve_array[sieve_array != 0]
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
@@ -26,4 +57,3 @@ if __name__ == '__main__':
     else:
         n =100
     print 'There are {0} primes less than {1}'.format(len(sieve2(n)), n)
-
