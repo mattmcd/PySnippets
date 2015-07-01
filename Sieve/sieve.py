ed93415b76901015a741c06102850fbac48360d7
mattmcd <matt@matt-mcdonnell.com>
2015-07-01 09:07:28 +0100

    Non-recursive version

--- Sieve/sieve.py
+++ Sieve/sieve.py
@@ -10,11 +10,20 @@ def sieve(n):
     aux(xrange(2,n))
     return acc
 
+def sieve2(n):
+    acc = []
+    numList = xrange(2,n)
+    while numList:
+        k = numList[0]
+        acc.append(k)
+        numList = filter(lambda x: x % k, numList)
+    return acc
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         n = int(sys.argv[1])
     else:
         n =100
-    print sieve(n)
+    print 'There are {0} primes less than {1}'.format(len(sieve2(n)), n)
 
